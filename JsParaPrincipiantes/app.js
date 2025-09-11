63a62fe3db4ec800b5132a83422f9fc0ef754809
Estre25 <estrellamarc250@gmail.com>
2025-09-11 00:06:31 -0600

    Operadores

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,13 +1,28 @@
-// variables
-var nameuser = "john";
+// Operadores
+let numberOne = 60;
+let numberTwo = 100;
+
+let result = numberOne + numberTwo;
+let result1 = numberOne - numberTwo;
+let result2 = numberOne * numberTwo;
+let result3 = numberOne / numberTwo;
+
+console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+
+let name = "john";
 let lastname = "carter";
+let completeName = name + " " + lastname;
+console.log(completeName);
 
-nameuser = "pepe";
+let numberOnee = 100;
+let numberTwoo = 500;
+let result4 = numberOnee > numberTwoo;
+console.log(result4);
 
-const PI = 3.1416;
-
-console.log(PI)
-
-//camelcase
-let userName = "gordon";
-/* */
+let passwordDB = "pepe123";
+let input = "pepe123";
+let result5 = input == passwordDB;
+console.log(result5);
