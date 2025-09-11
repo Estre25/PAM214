8bc456f666f651256a71266db5e6d36fb72115fd
Estre25 <estrellamarc250@gmail.com>
2025-09-10 18:14:58 -0600

    Historia de Javascript

---
+++ JsParaPrincipiantes/Video.java
@@ -0,0 +1,4 @@
+//En el año 1995 se creo JavaScript por la pelea de dos empresas una era Netscape y Microsoft cada una tenia un navegador,
+//querian que fueran las mas utilizadas por los usuarios y estaban agregando funcinalidad a sus navegadores
+// de manera que los programadores puedan crear sistios web de manera mas atractiva para el usuario,
+//y de esta manera Netscape creo JavaScript era que los desarrolladores pudieran hacer que los sitios o paginas web pudieran hacer mucho mas.
