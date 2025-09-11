e97b874f511c402313f8406ef2f70ad67fb6ab28
Estre25 <estrellamarc250@gmail.com>
2025-09-10 23:11:55 -0600

    Creación de una pagina web

--- JsParaPrincipiantes/Video.java
+++ JsParaPrincipiantes/Video.java
@@ -2,3 +2,17 @@
 //querian que fueran las mas utilizadas por los usuarios y estaban agregando funcinalidad a sus navegadores
 // de manera que los programadores puedan crear sistios web de manera mas atractiva para el usuario,
 //y de esta manera Netscape creo JavaScript era que los desarrolladores pudieran hacer que los sitios o paginas web pudieran hacer mucho mas.
+
+//para iniciar un codigo se necesitan algunas herramientas:
+//1.- Navegador
+//2.- Editor de código
+
+// Ir al navegador y escribir about: blank nos aparecera un la ventana en blanco,
+// ir hacia los tres puntos dar clic en mas herramientas y herramientas de desarrollador,
+// despues ir a console y escribir el codigo: console.log("hello world");->que se refire:consola muestrame el texto que esta dentro de los parentesis
+// y nos mandara: Hello world
+
+//Para modificar la pantalla colocamos el código: document.write("Hello world")
+//En la pantalla aparece: Hello World 
+
+
