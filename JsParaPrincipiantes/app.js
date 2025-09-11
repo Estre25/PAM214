414e50099f19264eb2d97422660c5c5d1ee39558
Estre25 <estrellamarc250@gmail.com>
2025-09-10 23:38:07 -0600

    Tipos de datos

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,1 +1,33 @@
-document.write("<h1>Hello world</h1");
+//tipos de datos
+"Hello world" //string
+'Hello world' //string
+console.log('this is a string')
+
+10000000//number
+-2.3
+console.log(14.3)
+
+//boolean
+true
+false
+console.log(true)
+
+//array
+['joey', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+console.log([1, 2, 3, 4])
+
+//object
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+}
+console.log({"username": "ryan",
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+});
