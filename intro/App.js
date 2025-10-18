11307af46cc6c6a20786d1b0c9e13dd74b22e627
Estre25 <estrellamarc250@gmail.com>
2025-10-17 19:54:56 -0600

    Importación de MenuScreen

--- intro/App.js
+++ intro/App.js
@@ -1,13 +1,11 @@
 //1. imports: Zona de importaciones
-import ContadorScreen from './screens/ContadorScreen';
+import MenuScreen from './screens/MenuScreen';
 
 //2.- Main: Zona de componentes
-export default function App() {
-
-
-
-  return (
-    <ContadorScreen></ContadorScreen>
+export default function App() 
+{
+ return (
+    <MenuScreen/>
   );
 }
 
