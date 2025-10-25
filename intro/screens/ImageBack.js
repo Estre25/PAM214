15b4280f244c2643241e92dd01446ec4bd312985
Estre25 <estrellamarc250@gmail.com>
2025-10-24 18:21:19 -0600

    ImageBackgroung & SlapshScreen: Completada

---
+++ intro/screens/ImageBack.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Button, SafeAreaView } from 'react-native';
+
+export default function Ejemplo() {
+
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false); 
+    }, 1000);
+    return () => clearTimeout(timer); 
+  }, []);
+
+   
+  if (showSplash) {
+    return (
+      <View style={styles.splashContainer}>
+        <Text style={styles.splashText}>Bienvenido a la App</Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        source={{
+          uri: 'https://wallpapercave.com/wp/wp3850825.jpg',
+        }} 
+        style={styles.background} 
+      >
+         <View style={styles.overlay}>
+
+          <Text style={styles.title}>Bienvenido!</Text>
+
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+// Estilos
+const styles = StyleSheet.create({
+  
+
+  splashContainer: {
+    flex: 1, 
+    backgroundColor: '#000000ff', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  
+  splashText: {
+    fontSize: 24, 
+    color: '#fff', 
+  },
+
+  
+  background: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    padding: 20, 
+    borderRadius: 10, 
+  },
+
+  
+  title: {
+    fontSize: 28, 
+    color: '#fff', 
+    marginBottom: 10, 
+    textAlign: 'center', 
+  },
+});
